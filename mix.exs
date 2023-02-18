3ffdfef35779e9dd619078880ea2a0c7c11da56c
vrtineu <vininro@gmail.com>
2023-02-18 19:51:12 -0300

    test: get max test coverage

--- mix.exs
+++ mix.exs
@@ -16,8 +16,7 @@ defmodule Exmeal.MixProject do
         coveralls: :test,
         "coveralls.detail": :test,
         "coveralls.post": :test,
-        "coveralls.html": :test,
-        "coveralls.json": :test
+        "coveralls.html": :test
       ]
     ]
   end
